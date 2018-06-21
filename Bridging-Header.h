e326b8808c19b24cad1ef234c3f42a72dc799790
Mak <mak@fish.razor.jp>
2018-06-21 23:23:33 +0900

    Initial commit

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  analytics-demo
+//
+//  Created by Makoto Shimizu on 2018/06/18.
+//  Copyright © 2018 mak. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "ADBMobile.h"
+
+#endif /* Bridging_Header_h */
--- analytics-demo/ViewController.swift
+++ analytics-demo/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import WebKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var webView: WKWebView!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ADBMobile.trackState("Top", data: nil)
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            ADBMobile.trackLocation(location, data: nil)
+        }
     }
 
-
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        var contextData: [String: String] = [:]
+        contextData["userID"] = "abc123"
+        
+        powerBtn.isHidden = true
+        
+        self.webView.isHidden = false
+            
+        let url = URL(string: "https://makoto-shimizu.com/")
+        let urlWithVisitorData = ADBMobile.visitorAppend(to: url)
+        let request = URLRequest(url: urlWithVisitorData!)
+        self.webView.load(request)
+        
+        ADBMobile.trackAction("WebView1", data: contextData)
+        
+    }
+    
 }
-
